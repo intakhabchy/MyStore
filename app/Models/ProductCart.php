48b67abb14ed6f6c2918995d2c6972eb40a541bd
intakhabchy <intakhab.chy@gmail.com>
2024-09-14 23:54:01 +0600

    class name correction

--- app/Models/ProductCart.php
+++ app/Models/ProductCart.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Product_Wishlist extends Model
+class ProductCart extends Model
 {
     use HasFactory;
 }
--- app/Models/ProductReview.php
+++ app/Models/ProductReview.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Product_Review extends Model
+class ProductReview extends Model
 {
     use HasFactory;
 }
